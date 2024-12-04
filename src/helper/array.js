0431c41b44e7e517a904d11963227abbfed8bc37
Topi <93841180+bknb@users.noreply.github.com>
2024-12-04 15:18:21 +0000

    update

---
+++ src/helper/array.js
@@ -0,0 +1,27 @@
+export const rng = (n, m) =>
+  [...Array(m - n)].map((_, i) => i + n);
+
+export const count = (arr, f = x=>x) =>
+  arr.filter(f).length;
+
+export const allBut = (arr, i) =>
+  arr.filter((x,j)=>j!==i);
+
+export const sum = (arr) =>
+  (typeof arr === 'object' ? arr : [...arguments])
+    .reduce((a, c) => a + c, 0);
+
+export const insertSorted = (arr, n) =>
+  arr.splice(getSortedIndex(arr,n), 0, n);
+
+function getSortedIndex(arr, n) {
+  let low = 0,
+      high = arr.length;
+
+  while (low < high) {
+    const mid = (low + high) >>> 1;
+    if (arr[mid] < n) low = mid + 1;
+    else high = mid;
+  }
+  return low;
+}
---
+++ src/helper/grid.js
@@ -0,0 +1,23 @@
+export const getEdges = (i,j,input) =>
+  [0,1].map(x=>[0,1].map(y=>
+    input[i+(x?1:-1)][j+(y?1:-1)])).flat();
+
+export const mapGrid = (input, func) =>
+  input.map((x,i)=>
+    x.map((y,j)=>
+      func(i,j,input)));
+
+export const sumGrid = (grid) =>
+  grid.reduce((a,c)=>
+    (a+c.reduce((a,c)=>
+      (a+c),0)),0);
+
+export const getNext = (c,dir) => {
+  switch(dir) {
+    case 0: c[0]--; case 1: c[1]++; break;
+    case 2: c[1]++; case 3: c[0]++; break;
+    case 4: c[0]++; case 5: c[1]--; break;
+    case 6: c[1]--; case 7: c[0]--; break;
+  }
+  return c;
+};
