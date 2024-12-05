9d2c60beda18b2eb074eb45f7386635148a8920d
Topi <93841180+bknb@users.noreply.github.com>
2024-12-05 07:23:01 +0000

    update

--- src/helper/array.js
+++ src/helper/array.js
@@ -1,5 +1,3 @@
-import { arrayBuffer } from "stream/consumers";
-
 export const rng = (n, m) =>
   [...Array(m - n)].map((_, i) => i + n);
 
@@ -16,17 +14,11 @@ export const sum = (arr) =>
 export const insert = (arr, n, i) =>
   [...arr.slice(0,i),n,...arr.slice(i)];
 
+const getSortedIndex = (arr, n) =>
+  [...arr].reduce(([low,high],c,i,a)=>
+    low>=high?a.splice(1)&&[low]:((mid)=>
+      arr[mid]<n?[mid+1,high]:[low,mid])
+    ((low + high) >>> 1),[0,arr.length])[0];
+
 export const insertSorted = (arr, n) =>
   insert(arr, n, getSortedIndex(arr,n));
-
-function getSortedIndex(arr, n) {
-  let low = 0,
-      high = arr.length;
-
-  while (low < high) {
-    const mid = (low + high) >>> 1;
-    if (arr[mid] < n) low = mid + 1;
-    else high = mid;
-  }
-  return low;
-}
