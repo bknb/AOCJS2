d3d755e6e53dba150f89449a03faadf738972b09
Topi <93841180+bknb@users.noreply.github.com>
2024-12-05 06:35:04 +0000

    update

--- solutions/24/5/solution.js
+++ solutions/24/5/solution.js
@@ -5,14 +5,14 @@ const sumMids = (input) =>
     .reduce((a,c)=>a+c);
 
 const checkLeft = (rules,p,pn,i) => 
-  rules.filter(([l])=>l===p)
+  rules.filter(([l])=>l==p)
     .every(([,r])=>(j => j<0 || j>i)
-      (pn.findIndex(p=>p===r)));
+      (pn.findIndex(p=>p==r)));
 
 const checkRight = (rules,p,pn,i) => 
-  rules.filter(([,r])=>r===p)
+  rules.filter(([,r])=>r==p)
     .every(([l])=>
-      pn.findIndex(p=>p===l) < i);
+      pn.findIndex(p=>p==l) < i);
 
 const correctOrder = (rules) =>
   (pn) => pn.every((p,i)=>
