e3335072e3f26b8c4ca1b3c1bf53a2129290e493
Topi <93841180+bknb@users.noreply.github.com>
2024-12-05 06:32:15 +0000

    update

--- solutions/24/5/solution.js
+++ solutions/24/5/solution.js
@@ -4,13 +4,6 @@ const sumMids = (input) =>
   input.map(r=>r[(r.length/2)|0])
     .reduce((a,c)=>a+c);
 
-export const part1 = ([rules,pn]) =>
-  sumMids(pn.filter(correctOrder(rules)));
-
-export const part2 = ([rules,pn]) => 
-  sumMids(pn.filter(p=>!correctOrder(rules)(p))
-    .map(bringInCorrectOrder(rules)));
-
 const checkLeft = (rules,p,pn,i) => 
   rules.filter(([l])=>l===p)
   .every(([,r])=>(j => j<0 || j>i)
@@ -20,17 +13,22 @@ const checkRight = (rules,p,pn,i) =>
   rules.filter(([,r])=>r===p)
   .every(([l])=>pn.findIndex(p=>p===l) < i);
 
-function correctOrder(rules) {
-  return (pn) => pn.every((p,i)=>
+const correctOrder = (rules) =>
+  (pn) => pn.every((p,i)=>
     checkLeft(rules,p,pn,i)
     && checkRight(rules,p,pn,i));
-}
 
-function bringInCorrectOrder(rules) {
-  return (pn) => pn.sort((a,b)=>
-    rules.find(([l,r])=>l===b&&r===a)?1
-    :(rules.find(([l,r])=>l===a&&r===b)?-1:0));
-}
+const bringInCorrectOrder = (rules) =>
+  (pn) => pn.sort((a,b)=>
+    rules.find(([l,r])=>l==b&&r==a)?1
+    :(rules.find(([l,r])=>l==a&&r==b)?-1:0));
+
+export const part1 = ([rules,pn]) =>
+  sumMids(pn.filter(correctOrder(rules)));
+
+export const part2 = ([rules,pn]) => 
+  sumMids(pn.filter(p=>!correctOrder(rules)(p))
+    .map(bringInCorrectOrder(rules)));
 
 export const init = (data) => 
   seperate(data)
