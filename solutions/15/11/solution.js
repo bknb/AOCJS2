821877de8dc7b2ac10ea1b049327da1d7104ae8a
Topi <93841180+bknb@users.noreply.github.com>
2024-12-05 15:40:04 +0000

    add 15 11

---
+++ solutions/15/11/solution.js
@@ -0,0 +1,35 @@
+const charCode = (pass) =>
+  pass.split('').map(x=>x.charCodeAt(0)-97);
+
+const checkAsc = pass => (parr =>
+  !parr.slice(1).reduce((a,c,i,arr)=>!a
+    ?arr.splice(1)&&a:(parr[i]+1===c?a+1:-2),-2))
+  (charCode(pass));
+
+const inc = (pass) => {
+  const chars = charCode(pass);
+  let over = true;
+  for(let i=chars.length;i-->0;)
+    if(over) over=!(chars[i]=(chars[i]+1)%26);
+    else break;
+  return String.fromCharCode(...chars.map(n=>n+97));
+}
+
+const checkReq = (pass) =>
+  !/[iol]/.test(pass) && pass.match(/(\w)\1/g)?.length>1;
+
+export const part1 = (input) => {
+  let pass = input;
+  while(!checkAsc(pass) || !checkReq(pass))
+    pass = inc(pass);
+  return pass;
+}
+
+export const part2 = (input) => {
+  let pass = inc(part1(input));
+  while(!checkAsc(pass) || !checkReq(pass))
+    pass = inc(pass);
+  return pass;
+}
+
+export const init = (data) => data;
