9d74220aff49b57a53b6cd2afea3c63aee34fb40
Topi <93841180+bknb@users.noreply.github.com>
2024-12-05 07:36:33 +0000

    update

--- solutions/24/4/solution.js
+++ solutions/24/4/solution.js
@@ -7,32 +7,30 @@ const count = (n, fn) =>
   (i,j,input) =>
     input[i][j]-n?0:fn(i,j,input);
 
+const check1 = (i,j,dir,input) =>
+  (input[i][j] === word.length-1)
+    || (([x,y]) => input[x]?.[y]
+      && input[x][y] === input[i][j]+1
+      && check1(x,y,dir,input))
+  (getNext([i,j],dir));
+
 const count1 = 
   count(0, (i,j,input) =>
     rng(0,8).filter(k=>
       check1(i,j,k,input)).length);
 
+const check2 = (i,j,input) =>
+  (i && i<input.length-1) 
+    && (edges => edges.every(x=>!isNaN(x)&&x%2)
+      && edges[0] !== edges[3] 
+      && (edges[0] === edges[1] 
+          ? edges[0] !== edges[2]
+          : edges[0] === edges[2]))
+    (getEdges(i,j,input));
+
 const count2 = 
   count(2, (i,j,input) =>
-    ~~check2(i,j,input));
-
-function check1(i,j,dir,input) {
-  if (input[i][j] === word.length-1) return true;
-  const [x,y] = getNext([i,j],dir);
-  if (!input[x]?.[y]) return false;
-  return input[x][y] === input[i][j]+1
-    && check1(x,y,dir,input);
-}
-
-function check2(i,j,input) {
-  if (!(i && i<input.length-1)) return false;
-  const edges = getEdges(i,j,input);
-  return edges.every(x=>!isNaN(x)&&x%2)
-    && edges[0] !== edges[3] 
-    && (edges[0] === edges[1] 
-        ? edges[0] !== edges[2]
-        : edges[0] === edges[2]);
-}
+    check2(i,j,input)|0);
 
 export const part1 = (input) => 
   sumGrid(mapGrid(input, count1));
