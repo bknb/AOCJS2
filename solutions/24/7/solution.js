088e04e72074ddb411827113529f3d0b02757ab8
Topi <93841180+bknb@users.noreply.github.com>
2024-12-07 12:00:35 +0000

    update

--- solutions/24/7/solution.js
+++ solutions/24/7/solution.js
@@ -1,4 +1,5 @@
 import {revCopy} from '#helper';
+import { log } from '#display';
 
 const opO = {
   '+': (a, b) => a + b,
@@ -6,13 +7,22 @@ const opO = {
   '||': (a, b) => +((''+a)+b),
 };
 
-export const part1 = (input) =>
-  input.map(numOfSols(['+','*']))
-  .reduce((a,[r,c])=>r?a+c:a,0);
+const red = {
+  '+': (r,s) => r>s&&r-s,
+  '*': (r,s) => !(r%s)&&r/s,
+  '||': (s,r) => (ss=>(rep=>ss!=rep&&+rep)
+    (ss.replace(new RegExp(`${''+r}$`),'')))(''+s)
+};
 
+export const part1 = (input) =>
+  input.map(hasSol(['+','*']))
+  .map((c,i)=>c?input[i][0]:0)
+  .reduce((a,c)=>(a+c),0);
+  
 export const part2 = (input) =>
-  input.map(numOfSols(['+','*','||']))
-  .reduce((a,[r,c])=>r?a+c:a,0);
+  input.map(hasSol(['+','*','||']))
+  .map((c,i)=>c?input[i][0]:0)
+  .reduce((a,c)=>(a+c),0);
 
 export const init = (data) =>
   data.split('\n').map(x=>
@@ -27,3 +37,14 @@ const allCombs = (opL,[s,...oR]) =>
   !oR.length?[s]:opL.map(o=>
     allCombs(opL,oR)
     .map(x=>opO[o](x,s))).flat();
+
+const hasSol = opL=>([result,list])=>
+  recOps(opL,opL,result,revCopy(list));
+
+const recOps = (ao,os,r,ns)=>
+  (([o,...oR],[n,...nR])=>
+    o&&(!nR.length
+    ?r==n:((rr=>rr&&recOps(ao,ao,rr,nR))
+    (red[o](r,n))||recOps(ao,oR,r,ns))))(os,ns);
+  
+  
