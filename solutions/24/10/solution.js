0157aedfcb5e825d08e88cb1bc7523a75d1c3204
Topi <93841180+bknb@users.noreply.github.com>
2024-12-10 07:13:16 +0000

    update

--- solutions/24/10/solution.js
+++ solutions/24/10/solution.js
@@ -2,32 +2,31 @@ import {mapGrid, sumGrid, allNext,oob} from '#helper';
 import {gridWise, toNum} from '#parser';
 
 export const part1 = (input) => 
-  sumGrid(mapGrid(input,trailHead));
+  trailHead(input,true);
 
-const trailHead = (c,x,y,grid) =>
-  c==0?countTH(0,[[x,y]],grid).length:0;
+export const part2 = (input) => 
+  trailHead(input,false);
 
-const trailRating = (c,x,y,grid) =>
-  c==0?countTH2(0,[[x,y]],grid).length:0;
+export const init = (data) => 
+  gridWise(toNum)(data)
 
-const countTH2 = (c,l,grid) =>
-  c==9?l:countTH2(c+1,
-  l.map(([x,y])=>stepUp(c,x,y,grid)).flat(),grid);
+const trailHead = (grid,f) =>
+  sumGrid(mapGrid(grid,(c,x,y)=>
+    c==0?countTH(0,[[x,y]],grid,f).length:0));
 
-const countTH = (c,l,grid) =>
-  c==9?l:countTH(c+1,
-  l.map(([x,y])=>stepUp(c,x,y,grid)).flat()
-    .filter(([a,b],i,arr)=>
-      !arr.slice(0,i).some(([c,d])=>a==c&&b==d)),grid);
+const countTH = (c,l,grid,f) =>
+  c==9?l:countTH(
+    c+1,nextLayer(
+      l.map(([x,y])=>
+        stepUp(c,x,y,grid))
+      .flat(),f),grid,f);
+
+const nextLayer = (su,f) =>
+  !f?su:su.filter(([a,b],i,arr)=>!arr.slice(0,i)
+    .some(([c,d])=>a==c&&b==d));
 
 const stepUp = (c, x, y, grid) => 
   allNext([x,y])
     .filter((_,i)=>!(i%2))
     .filter(x=>!oob(...x,grid))
     .filter(([x,y])=>grid[x][y]==c+1);
-
-export const part2 = (input) => 
-  sumGrid(mapGrid(input,trailRating));
-
-export const init = (data) => 
-  gridWise(toNum)(data)
