7a74c38eef0af9c646383ee587f3cfdd78a1eaa6
Topi <93841180+bknb@users.noreply.github.com>
2024-12-11 13:14:37 +0000

    add 17 2

---
+++ solutions/17/2/solution.js
@@ -0,0 +1,18 @@
+import {log} from '#display';
+import * as helper from '#helper';
+import * as parser from '#parser';
+
+export const part1 = (input) =>
+  input.map(r=>r.reduce((a,c)=>Math.max(a,c))
+    -r.reduce((a,c)=>Math.min(a,c)))
+  .reduce((a,c)=>a+c,0);
+
+export const part2 = (input) => 
+  input.slice().map(r=>
+    r.reduce((a,c,i,arr)=>a
+      ?arr.splice(1)&&a
+      :(sr=>sr?c/sr:0)(arr.find((x,j)=>i!==j&&!(c%x))),0))
+  .reduce((a,c)=>a+c,0);
+
+export const init = (data) => 
+  data.split('\n').map(r=>r.match(/\d+/g).map(x=>+x));
