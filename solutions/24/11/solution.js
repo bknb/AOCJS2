64eebe671471fd6351a749dc41dd7b28428b448b
Topi <93841180+bknb@users.noreply.github.com>
2024-12-11 09:43:35 +0000

    update

--- solutions/24/11/solution.js
+++ solutions/24/11/solution.js
@@ -1,30 +1,29 @@
 const times1 = 25;
 const times2 = 75;
-
 const cache = new Map();
 
 export const part1 = input=>addAllStones(input,times1);
 
 export const part2 = input=>addAllStones(input,times2);
 
-export const init = data=>data.split(' ');
+export const init = data=>data.split(' ').map(x=>+x);
 
 const addAllStones = (stones, blinks)=>
   stones.reduce((a,c)=>a+ev(blinks)(c),0);
 
-const ev = n=>stone => {
+const ev = n=>s=> {
   if(!n) return 1;
-  const key = `${stone},${n}`;
-  if(cache.has(key)) return cache.get(key);
-  let ns = [];
-  const sl = stone.length;
-  if(!+stone) ns.push('1');
-  else if(sl%2) ns.push(''+stone*2024);
-  else ns.push(
-    stone.substring(0,(sl/2)|0),
-    ''+(+stone.substring((sl/2)|0)));
-  const res = ns.map(ev(n-1))
+  const key = `${s},${n}`;
+  if(cache.has(key))
+    return cache.get(key);
+  const res = next(s).map(ev(n-1))
     .reduce((a,c)=>a+c);
   cache.set(key,res);
   return res;
 }
+
+const next = (s,ss=''+s)=>
+  ((sl=ss.length,hsl=(sl/2)|0)=>
+  !+s?[1]:(sl%2?[s*2024]
+      :[+ss.substring(0,hsl),
+        +ss.substring(hsl)]))();
