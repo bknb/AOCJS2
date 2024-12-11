1b459b9fa683496b3eb9865d9b43e9567414867b
Topi <93841180+bknb@users.noreply.github.com>
2024-12-11 13:15:13 +0000

    update

--- solutions/17/2/solution.js
+++ solutions/17/2/solution.js
@@ -1,7 +1,3 @@
-import {log} from '#display';
-import * as helper from '#helper';
-import * as parser from '#parser';
-
 export const part1 = (input) =>
   input.map(r=>r.reduce((a,c)=>Math.max(a,c))
     -r.reduce((a,c)=>Math.min(a,c)))
@@ -11,7 +7,9 @@ export const part2 = (input) =>
   input.slice().map(r=>
     r.reduce((a,c,i,arr)=>a
       ?arr.splice(1)&&a
-      :(sr=>sr?c/sr:0)(arr.find((x,j)=>i!==j&&!(c%x))),0))
+      :(sr=>sr?c/sr:0)
+      (arr.find((x,j)=>
+        i!==j&&!(c%x))),0))
   .reduce((a,c)=>a+c,0);
 
 export const init = (data) => 
