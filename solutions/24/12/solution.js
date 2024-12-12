ade6cb082a07e6e6579856b7a1759049d7742add
Topi <93841180+bknb@users.noreply.github.com>
2024-12-12 10:36:10 +0000

    update

--- solutions/24/12/solution.js
+++ solutions/24/12/solution.js
@@ -1,16 +1,16 @@
 import {log} from '#display';
-import {uniqChars, allNext, bordered, rng} from '#helper';
+import {uniqChars, allNext, bordered, rng, count} from '#helper';
 import {gridify} from '#parser';
 
 export const part1 = (input) => 
-  solve(input,costPeri)
+  solve(input,fences)
 
 export const part2 = (input) => 
-  solve(input,costSides);
+  solve(input,corners);
 
-const solve = ([plants,grid],costFn) =>
+const solve = ([plants,grid],countFn) =>
   [...getAreas(plants,grid).values()]
-    .map(x=>x.map(costFn)
+    .map(x=>x.map(cost(countFn,([x,y])=>grid[x][y]))
     .reduce((a,c)=>a+c))
     .reduce((a,c)=>a+c);
 
@@ -18,39 +18,30 @@ export const init = (data) =>
   [uniqChars(data).filter(x=>x!='\n'),
    bordered(gridify(data))];
 
-const costSides = fs=> {
-  const isin=([i,j])=>
-    fs.some(([x,y])=>i==x&&j==y);
-  const sides = fs.map(c=>
-    ((ns=allNext(c))=>
-      rng(0,4).reduce((a,c)=>
-        ((i,ni=(i+2)%8)=>(isin(ns[i])
-          ?(isin(ns[ni])&&!isin(ns[i+1]))
-          :!isin(ns[ni]))?a+1:a)
-        (c*2),0))()).reduce((a,c)=>a+c);
-  return fs.length*sides;
-}
+const cost = (fn,v)=>fs=>
+  fs.length*fs.map(fn(v))
+    .reduce((a,c)=>a+c);;
 
-const costPeri = fs=> {
-  const isin=([i,j])=>
-    fs.some(([x,y])=>i==x&&j==y);
-  const fences = fs.map(c=>
-    allNext(c,true)
-    .filter(x=>!isin(x)).length)
-    .reduce((a,c)=>a+c);
-  return fs.length*fences;
-}
+const corners = v=>x=>
+  ((ns=allNext(x),cf=v(x))=>
+    rng(0,4).reduce((a,c)=>
+      ((i,ni=(i+2)%8)=>
+        (v(ns[i])!=cf)==(v(ns[ni])!=cf)
+        &&(cf!=v(ns[i+1])||v(ns[i])!=cf)
+        ?a+1:a)
+      (c*2),0))();
 
-const getArea = (c,i,j,grid,fs) => {
-  const sij=i+','+j;
-  if (grid[i][j]==c && !fs.has(sij)) {
-    fs.add(sij);
-    allNext([i,j],true)
-      .forEach(([x,y])=>
-        getArea(c,x,y,grid,fs));
-  }
-  return fs;
-}
+const fences = v=>c=>
+    count(allNext(c,true),x=>v(x)!=v(c));
+
+const getArea = (c,i,j,grid,fs) =>
+  ((sij=i+','+j)=>
+    (grid[i][j]==c&&!fs.has(sij)
+     ?fs.add(sij)
+     &&allNext([i,j],true)
+     .forEach(([x,y])=>
+       getArea(c,x,y,grid,fs))
+     ||fs:fs))();
 
 const getAreas = (plants,grid)=> {
   const areas = new Map();
