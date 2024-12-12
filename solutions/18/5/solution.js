decdff9f1538c5c996ad047ad6289aebc4467e37
Topi <93841180+bknb@users.noreply.github.com>
2024-12-12 17:16:36 +0000

    add 18 5

---
+++ solutions/18/5/solution.js
@@ -0,0 +1,22 @@
+import {log} from '#display';
+import * as helper from '#helper';
+import * as parser from '#parser';
+
+const alph = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+export const part1 = (input) => {
+  let last = input;
+  do {
+    input = alph.reduce((a,c)=>a.replace(c+c.toUpperCase(),''),input);
+    input = alph.reduce((a,c)=>a.replace(c.toUpperCase()+c,''),input);
+  } while (last != (last = input));
+  return input.length;
+}
+
+export const part2 = (input) => 
+  alph.reduce((a,c)=>
+    Math.min(a,part1(
+      input.replaceAll(c,'')
+      .replaceAll(c.toUpperCase(),''))),Infinity);
+
+export const init = (data) => data
