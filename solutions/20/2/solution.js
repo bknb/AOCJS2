330d0abac136596834f1dc5995df4063f97d81a2
Topi <93841180+bknb@users.noreply.github.com>
2024-12-12 16:23:25 +0000

    add 20 2

---
+++ solutions/20/2/solution.js
@@ -0,0 +1,13 @@
+import {log} from '#display';
+import {count, checkCharNM} from '#helper';
+
+export const part1 = (input) => 
+  count(input,([min,max,letter,password])=>
+    checkCharNM(min,max,letter,password));
+
+export const part2 = (input) => 
+  count(input,([n,m,letter,password])=>
+    (password[n-1]==letter)!=(password[m-1]==letter))
+
+export const init = (data) => 
+  data.split('\n').map(x=>x.match(/\w+/g));
--- src/helper/string.js
+++ src/helper/string.js
@@ -1,2 +1,7 @@
 export const uniqChars = (str) =>
-  [...new Set(str.split(''))]
+  [...new Set(str.split(''))];
+
+export const checkCharNM = (min,max,letter,str)=>
+  ((l=letter,nl=`[^${l}]*`)=>
+    new RegExp(`^${nl}(${l}${nl}){${min},${max}}${nl}$`))()
+  .test(str);
