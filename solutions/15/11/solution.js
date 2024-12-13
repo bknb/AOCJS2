f34a69575509d982fd7d799c40cfb7a8f26ed5e6
Topi <93841180+bknb@users.noreply.github.com>
2024-12-13 09:41:58 +0000

    update

--- solutions/15/11/solution.js
+++ solutions/15/11/solution.js
@@ -1,12 +1,24 @@
-const charCode = (pass) =>
+export const part1 = input=> {
+  let pass = input;
+  while(!checkAsc(pass) || !checkReq(pass))
+    pass = inc(pass);
+  return pass;
+}
+
+export const part2 = input=>
+  part1(inc(part1(input)));
+
+export const init = data=>data;
+
+const charCode = pass=>
   pass.split('').map(x=>x.charCodeAt(0)-97);
 
-const checkAsc = pass => (parr =>
+const checkAsc = pass=> (parr=>
   !parr.slice(1).reduce((a,c,i,arr)=>!a
     ?arr.splice(1)&&a:(parr[i]+1===c?a+1:-2),-2))
   (charCode(pass));
 
-const inc = (pass) => {
+const inc = pass=> {
   const chars = charCode(pass);
   let over = true;
   for(let i=chars.length;i-->0;)
@@ -17,19 +29,3 @@ const inc = (pass) => {
 
 const checkReq = (pass) =>
   !/[iol]/.test(pass) && pass.match(/(\w)\1/g)?.length>1;
-
-export const part1 = (input) => {
-  let pass = input;
-  while(!checkAsc(pass) || !checkReq(pass))
-    pass = inc(pass);
-  return pass;
-}
-
-export const part2 = (input) => {
-  let pass = inc(part1(input));
-  while(!checkAsc(pass) || !checkReq(pass))
-    pass = inc(pass);
-  return pass;
-}
-
-export const init = (data) => data;
