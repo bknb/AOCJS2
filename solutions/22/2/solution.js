337eb34efc4ba9afeafbeef4c68293ba2801d4a2
Topi <93841180+bknb@users.noreply.github.com>
2024-12-13 09:26:23 +0000

    update

--- solutions/22/2/solution.js
+++ solutions/22/2/solution.js
@@ -2,19 +2,17 @@ import {log} from '#display';
 import * as helper from '#helper';
 import * as parser from '#parser';
 
-const l1 = 'ABC';
-const l2 = 'XYZ';
+const l = ['ABC','XYZ'];
 
 export const part1 = (input) => 
   input.map(([a,b])=>
-    ((ai=l1.indexOf(a),bi=l2.indexOf(b))=>
-      1+bi+3*(ai==bi||((ai!=((bi+1)%3))&&2)))())
+    1+b+3*(a==b||((a!=((b+1)%3))&&2)))
   .reduce((a,b)=>a+b);
 
 export const part2 = (input) => 
   part1(input.map(([a,b])=>
-    ((ai=l1.indexOf(a),bi=l2.indexOf(b))=>
-      [a,l2[(ai+(bi?((bi-1)?1:0):2))%3]])()));
+    [a,(a+(b?((b-1)?1:0):2))%3]));
 
 export const init = (data) => 
-  data.split('\n').map(r=>r.split(' '));
+  data.split('\n').map(r=>
+    r.split(' ').map((x,i)=>l[i].indexOf(x)));
