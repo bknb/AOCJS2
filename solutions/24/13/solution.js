3ac9347ecb36577356ae530b4fc21ea59bbc2f72
Topi <93841180+bknb@users.noreply.github.com>
2024-12-13 10:42:40 +0000

    update

--- solutions/24/13/solution.js
+++ solutions/24/13/solution.js
@@ -10,19 +10,22 @@ export const part2 = (input) =>
     [x,y,p.map(c=>c+inc)]));
 
 const solve = (input)=>
-  input.map(
-    ([[x1,x2],[y1,y2],[p1,p2]])=> {
-      const c1 = p1*x2-p2*x1;
-      const c2 = y1*x2-y2*x1;
-      if (c1%c2) return 0n;
-      const b = c1/c2;
-      const d = p1-b*y1;
-      if (d%x1) return 0n;
-      const a = d/x1;
-      return a*3n+b;
-    }).reduce((a,c)=>a+c);
+  input.map(getTokens)
+  .reduce((a,c)=>a+c);
 
 export const init = (data) => 
   seperate(data).map(g=>
     g.map(l=>l.match(/\d+/g)
       .map(x=>BigInt(x))));
+
+const getTokens=
+  ([[x1,x2],[y1,y2],[p1,p2]])=> {
+    const c1 = p1*x2-p2*x1;
+    const c2 = y1*x2-y2*x1;
+    if (c1%c2) return 0n;
+    const b = c1/c2;
+    const d = p1-b*y1;
+    if (d%x1) return 0n;
+    const a = d/x1;
+    return a*3n+b;
+  }
