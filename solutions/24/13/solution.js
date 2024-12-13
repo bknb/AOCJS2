38fdaae651fb0bf9a71fe60e92d2b21e5bb05bce
Topi <93841180+bknb@users.noreply.github.com>
2024-12-13 06:05:52 +0000

    update

--- solutions/24/13/solution.js
+++ solutions/24/13/solution.js
@@ -4,8 +4,8 @@ import {seperate,lineWise,toNum} from '#parser';
 
 export const part1 = (input) =>
   input.map(([[x1,x2],[y1,y2],[p1,p2]],i)=> {
-    const c1 = (p1*x2-p2*x1)
-    const c2 = (y1*x2-y2*x1);
+    const c1 = p1*x2-p2*x1
+    const c2 = y1*x2-y2*x1;
     if (c1 % c2) return 0;
     const b = c1 / c2;
     const a = (p1-b*y1)/x1;
@@ -19,8 +19,8 @@ export const part2 = (input) =>
     const [y1,y2] = toBigInt(y);
     const [p1,p2] = toBigInt(p)
       .map(x=>x+10000000000000n);
-    const c1 = (p1*x2-p2*x1)
-    const c2 = (y1*x2-y2*x1);
+    const c1 = p1*x2-p2*x1
+    const c2 = y1*x2-y2*x1;
     if (c1 % c2) return 0n;
     const b = c1 / c2;
     const a = (p1-b*y1)/x1;
