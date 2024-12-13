fd94a629717b7a2abbf6cfe0d57df664e5b704eb
Topi <93841180+bknb@users.noreply.github.com>
2024-12-13 09:28:29 +0000

    update

--- solutions/15/10/solution.js
+++ solutions/15/10/solution.js
@@ -1,3 +1,11 @@
+export const part1 = (input) =>
+  recNext(input,40);
+
+export const part2 = (input) =>
+  recNext(input,50);
+
+export const init = (data) => data;
+
 const next = (ns) =>
   ns.replace(/([1-3])\1*/g, (a,b)=>
     a.length+b);
@@ -6,11 +14,3 @@ const recNext = (ns,n) =>
   n?ns.split(/(?<=2)(?=13[12])/)
     .map(nsc=>recNext(next(nsc),n-1))
     .reduce((a,c)=>a+c):ns.length;
-
-export const part1 = (input) =>
-  recNext(input,40);
-
-export const part2 = (input) =>
-  recNext(input,50);
-
-export const init = (data) => data
