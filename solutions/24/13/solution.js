b6f53ab4679dc86222006dacd74b4b3e424df70a
Topi <93841180+bknb@users.noreply.github.com>
2024-12-13 07:37:23 +0000

    update

--- solutions/24/13/solution.js
+++ solutions/24/13/solution.js
@@ -2,33 +2,25 @@ import {debug} from '#display';
 import {seperate,lineWise,toNum} from '#parser';
 
 export const part1 = (input) =>
-  input.map(([[x1,x2],[y1,y2],[p1,p2]],i)=> {
-    const c1 = p1*x2-p2*x1
-    const c2 = y1*x2-y2*x1;
-    if (c1 % c2) return 0;
-    const b = c1 / c2;
-    const d = p1-b*y1;
-    if (d % x1) return 0;
-    const a = d/x1;
-    debug(a,b,i);
-    return a*3+b;
-  }).reduce((a,c)=>a+c);
+  solve(input);
 
 export const part2 = (input) => 
-  input.map(([x,y,p],i)=> {
-    const [x1,x2] = toBigInt(x);
-    const [y1,y2] = toBigInt(y);
-    const [p1,p2] = toBigInt(p)
-      .map(x=>x+10000000000000n);
+  solve(input.map(([x,y,p])=>
+    [x,y,p.map(c=>c+10000000000000)]),true);
+
+const solve = (input,big)=> 
+  input.map(([x,y,p])=> {
+    const [x1,x2] = big?toBigInt(x):x;
+    const [y1,y2] = big?toBigInt(y):y;
+    const [p1,p2] = big?toBigInt(p):p;
     const c1 = p1*x2-p2*x1;
     const c2 = y1*x2-y2*x1;
-    if (c1 % c2) return 0n;
+    if (c1%c2) return big?0n:0;
     const b = c1 / c2;
     const d = p1-b*y1;
-    if (d % x1) return 0n;
+    if (d%x1) return big?0n:0;
     const a = d/x1;
-    debug(a,b,i);
-    return a*3n+b;
+    return a*(big?3n:3)+b;
   }).reduce((a,c)=>a+c);
 
 const toBigInt = v => v.map(x=>BigInt(x));
