d4e89b55486ba6e43a0ecbb65c23942c766a6b22
Topi <93841180+bknb@users.noreply.github.com>
2024-12-13 07:39:32 +0000

    update

--- solutions/24/13/solution.js
+++ solutions/24/13/solution.js
@@ -6,21 +6,21 @@ export const part1 = (input) =>
 
 export const part2 = (input) => 
   solve(input.map(([x,y,p])=>
-    [x,y,p.map(c=>c+10000000000000)]),true);
+    [x,y,p.map(c=>c+10000000000000)]));
 
-const solve = (input,big)=> 
+const solve = (input)=>
   input.map(([x,y,p])=> {
-    const [x1,x2] = big?toBigInt(x):x;
-    const [y1,y2] = big?toBigInt(y):y;
-    const [p1,p2] = big?toBigInt(p):p;
+    const [x1,x2] = toBigInt(x);
+    const [y1,y2] = toBigInt(y);
+    const [p1,p2] = toBigInt(p);
     const c1 = p1*x2-p2*x1;
     const c2 = y1*x2-y2*x1;
-    if (c1%c2) return big?0n:0;
+    if (c1%c2) return 0n;
     const b = c1 / c2;
     const d = p1-b*y1;
-    if (d%x1) return big?0n:0;
+    if (d%x1) return 0n;
     const a = d/x1;
-    return a*(big?3n:3)+b;
+    return a*3n+b;
   }).reduce((a,c)=>a+c);
 
 const toBigInt = v => v.map(x=>BigInt(x));
