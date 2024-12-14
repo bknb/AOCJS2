6b03c585b756895f2f0dc75bd4ee4dd156cf8df7
Topi <93841180+bknb@users.noreply.github.com>
2024-12-14 06:40:29 +0000

    update

--- solutions/24/14/solution.js
+++ solutions/24/14/solution.js
@@ -1,45 +1,38 @@
-import {log, debug} from '#display';
 import {mod} from '#helper';
 import {isTest} from '#solver';
 
-let w,h;
+let w,h,wh,hh;
 
-export const part1 = (input) => {
-  const rs=solve(input,100);
-  const wh = w>>1;
-  const hh = h>>1;
-  return debug(rs.reduce(([a,b,c,d],[x,y])=>{
-    if (x<wh) {
-      if (y<hh) return [a+1,b,c,d];
-      else if (y>hh) return [a,b,c+1,d];
-    } else if (x>wh) {
-      if (y<hh) return [a,b+1,c,d];
-      else if (y>hh) return [a,b,c,d+1];
-    }
-    return [a,b,c,d];
-  },[0,0,0,0])).reduce((a,b)=>a*b);
-};
+export const part1 = input=>
+  solve(input,100)
+    .reduce(([a,b,c,d],[x,y])=>
+      (x<wh?
+       (y<hh?
+        [++a,b,c,d]
+        :y>hh&&
+        [a,b,++c,d])
+       :x>wh&&
+       (y<hh?
+        [a,++b,c,d]
+        :y>hh&&
+        [a,b,c,++d]))
+      ||[a,b,c,d],[0,0,0,0])
+    .reduce((a,b)=>a*b);
 
 const solve = (input, times) =>
-  input.map(([x,y,dx,dy])=>{
-    x += dx*times;
-    y += dy*times;
-    x=mod(x,w);
-    y=mod(y,h);
-    return [x,y];
-  });
+  input.map(([x,y,dx,dy])=>
+    [mod(x+dx*times,w),mod(y+dy*times,h)]);
 
-export const part2 = (input) => {
-  let t=0;
-  let rs=input;
-  while (rs.some(([x,y],i,arr)=>
-    arr.slice(i+1).some(([x2,y2])=>x2==x&&y2==y))) {
-    rs=solve(input,++t);
-  }
-  return t;
-};
+export const part2 = input=>
+  recPart2(input,1)
 
-export const init = (data) => {
-  [w,h] = isTest()?[11,7]:[101,103];
-  return data.split('\n').map(r=>r.match(/[-\d]+/g).map(x=>+x));
-}
+const recPart2 = (input,t) =>
+  solve(input,t).some(([x,y],i,arr)=>
+    arr.slice(i+1).some(([x2,y2])=>x2==x&&y2==y))
+  &&(recPart2(input,++t)||t);
+
+export const init = (data) =>
+  ([w,h]=isTest()?[11,7]:[101,103])
+    &&([wh,hh]=[w>>1,h>>1])
+    &&data.split('\n').map(r=>
+      r.match(/[-\d]+/g).map(x=>+x));
