ca9084a0603d19fadabdff41e679c742120e555a
Topi <93841180+bknb@users.noreply.github.com>
2024-12-17 09:11:07 +0000

    update

--- solutions/24/17/solution.js
+++ solutions/24/17/solution.js
@@ -1,43 +1,55 @@
-import {log} from '#display';
 import {seperate} from '#parser';
 
 export const part1 = ([reg,pro]) => {
   const out = [];
-  for (let i = 0; i < pro.length; i+=2) {
-    const code = pro[i];
-    const val = pro[i+1];
-    //log(i,code,val);
-    switch(code) {
-      case 0: reg[0]>>=getVal(val,reg); break;
-      case 1: reg[1]^=val; break;
-      case 2: reg[1]=getVal(val,reg)%8; break;
-      case 3: reg[0]&&(i=val-2); break;
-      case 4: reg[1]^=reg[2]; break;
-      case 5: out.push(getVal(val,reg)%8); break;
-      case 6: reg[1]=reg[0]>>getVal(val,reg); break;
-      case 7: reg[2]=reg[0]>>getVal(val,reg); break;
-      default:
-    }
-  }
-  log(reg);
+  reg = [...reg];
+  for (let i = 0n; i < pro.length; i+=2n)
+    i=calcStep(reg,pro,i,out);
   return out.join(',');
 };
 
+const calcStep= (reg,pro,i,out) => {
+  const code = pro[i];
+  const val = pro[i+1n];
+  switch(code) {
+    case 0n: reg[0]>>=getVal(val,reg); break;
+    case 1n: reg[1]^=val; break;
+    case 2n: reg[1]=getVal(val,reg)%8n; break;
+    case 3n: reg[0]&&(i=val-2n); break;
+    case 4n: reg[1]^=reg[2]; break;
+    case 5n: out.push(getVal(val,reg)%8n); break;
+    case 6n: reg[1]=reg[0]>>getVal(val,reg); break;
+    case 7n: reg[2]=reg[0]>>getVal(val,reg); break;
+    default:
+  }
+  return i;
+}
+
 const getVal = (val, reg) => {
   switch (val) {
-    case 0:case 1:case 2:case 3: return val;
-    case 4:case 5:case 6: return reg[val-4];
+    case 0n:case 1n:case 2n:case 3n: return val;
+    case 4n:case 5n:case 6n: return reg[val-4n];
     default:
   }
 }
-
+  
 export const part2 = ([reg,pro]) => {
-  // Write your code here
-  return null;
+  const chunks = pro.map(_=>0);
+  for (let i = 0; i < pro.length; i++) {
+    for (let j=8; j-->0;) {
+      const regC = reg.slice();
+      regC[0]=chunks.reduce((a,c)=>a=(a<<3n)^BigInt(c),0n);
+      const out = part1([regC,pro]).split(',').map(BigInt);
+      if (out[pro.length-1-i]==pro[pro.length-1-i])
+        break;
+      chunks[i]++;
+    }
+  }
+  return chunks.reduce((a,c)=>a=(a<<3n)^BigInt(c),0n);
 };
 
 export const init = (data) => {
   const [reg,pro] = seperate(data);
-  return [reg.map(x=>+x.match(/(\d+)/)[0]),
-          pro[0].split(' ')[1].split(',').map(x=>+x)];
+  return [reg.map(x=>BigInt(x.match(/(\d+)/)[0])),
+          pro[0].split(' ')[1].split(',').map(x=>BigInt(x))];
 };
