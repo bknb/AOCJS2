0d0e4a1a1d6dc242ee6a566053112597482944f7
topi <bknb@wakatipi.de>
2024-12-19 22:55:09 +0100

    added 24 20 boiler

---
+++ solutions/24/20/solution.js
@@ -0,0 +1,18 @@
+import {log} from '#display';
+import * as helper from '#helper';
+import * as parser from '#parser';
+
+export const part1 = (input) => {
+  // Write your code here
+  return null;
+};
+
+export const part2 = (input) => {
+  // Write your code here
+  return null;
+};
+
+export const init = (data) => {
+  // Write your code here
+  return null;
+};
