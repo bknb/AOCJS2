15406703bd7bbf3bb6aa4a0f3e083085c1da3d87
topi <bknb@wakatipi.de>
2025-12-01 08:12:39 +0100

    update

--- solutions/25/1/solution.js
+++ solutions/25/1/solution.js
@@ -4,16 +4,19 @@ import {linify, toNum} from '#parser';
 const a = 100;
 const s = 50;
 
-const nd = (isL,d,n) => mod(isL?d-n:d+n,a);
-const ro = (isL,d,n) => isL?(n%a)>a-d:(n%a)>d;
+const nd = (isL,d,n) => 
+  mod(isL ? d-n : d+n, a);
+const ro = (isL,d,n,na=n%a) => 
+  isL ? na > a-d : na > d;
 
 export const part1 = (input, d = s) => 
   input.reduce((c,[isL,n]) =>
-    c + ~~!(d=nd(isL,d,n)), 0);
+    ~~!(d = nd(isL,d,n)) + c, 0);
 
 export const part2 = (input, d = s) => 
   input.reduce((c,[isL,n]) =>
-    c + ~~(n/a) + (~~!(d=nd(isL,d,n)) || ro(isL,d,n)), 0);
+    (~~!(d = nd(isL,d,n)) || ro(isL,d,n))
+    + ~~(n/a) + c, 0);
 
 export const init = (data) => 
   linify(data)
