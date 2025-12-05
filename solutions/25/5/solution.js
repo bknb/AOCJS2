832ea0432e587a8fe996478cb7ffa26cb3364b7e
topi <bknb@wakatipi.de>
2025-12-05 07:07:28 +0100

    update

--- solutions/25/5/solution.js
+++ solutions/25/5/solution.js
@@ -1,12 +1,14 @@
-export const part1 = ([rs,l]) => 
+export const part1 = ([rs,l]) =>
   l.filter(n=>rs.some(([l,r])=>
     l<=n&&n<=r)).length;
 
-export const part2 = ([rs]) => 
+export const part2 = ([rs]) =>
   rs.sort(([a],[b])=>a-b)
     .reduce(([[fl,fr],...rr],[l,r])=>
-      l>fr+1?[[l,r],[fl,fr],...rr]
-      :[[fl,r>fr?r:fr],...rr],[[0,-1]])
+      l>fr+1
+        ?[[l,r],[fl,fr],...rr]
+        :[[fl,r>fr?r:fr],...rr]
+      ,[[0,-1]])
     .reduce((s,[l,r])=>r-l+1+s,0);
 
 export const init = (data) =>
