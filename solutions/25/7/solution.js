0ad1c7e2bf11374e9601dd3c581067114b145b91
topi <bknb@wakatipi.de>
2025-12-07 06:54:56 +0100

    update

--- solutions/25/7/solution.js
+++ solutions/25/7/solution.js
@@ -1,26 +1,22 @@
-import {debug, log, logGrid} from '#display';
 import {cached} from '#helper';
 import {gridify} from '#parser';
 
-export const part1 = ([s,g]) => {
-  let splits = 0;
-  const cs = new Set([s]);
-  g.forEach(r=>
-    r.forEach((c,i)=>{
-      if(c==='^'&&cs.has(i)) {
-        ++splits;
-        cs.delete(i);
-        cs.add(i+1).add(i-1);
-      }
-    }));
-  return splits;
-};
+export const part1 = ([s,g]) => 
+  (cs=>g.reduce((ts,r)=>
+    r.reduce((s,c,i)=>
+      (cs.has(i)&&c==='^'
+        &&cs.delete(i)
+        &&cs.add(i+1).add(i-1)
+        &&s+1)||s,0)+ts,0))
+  (new Set([s]));
 
 export const part2 = ([s,g]) => {
   const solve = cached((i,j) => {
-    if (i===g.length) return 1;
-    if (g[i][j]==='.') return solve(i+1,j);
-    if (g[i][j]==='^') return solve(i+1,j-1)+solve(i+1,j+1);
+    switch(g?.[i]?.[j]) {
+      case '.': return solve(i+1,j);
+      case '^': return solve(i+1,j-1)+solve(i+1,j+1);
+      default: return 1;
+    }
   });
   return solve(0,s);
 };
