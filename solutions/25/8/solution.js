b57c00206d6b4276b632f69a49e85a5bfc4b3472
topi <bknb@wakatipi.de>
2025-12-08 07:51:32 +0100

    update

--- solutions/25/8/solution.js
+++ solutions/25/8/solution.js
@@ -1,7 +1,4 @@
-import {debug, log, condLog} from '#display';
 import { isTest } from '#solver';
-import * as helper from '#helper';
-import * as parser from '#parser';
 
 const dist = (a,b) => {
   let sum = 0;
@@ -18,42 +15,38 @@ const getAllDist = input => {
   return ds;
 }
 
-export const part1 = (input) => {
-  const all = getAllDist(input).sort(([a],[b])=>a-b);
-  const js = input.map((_,i)=>new Set([i]));
-  let max = isTest()?10:1000;
-  for (let k=0;k<max;k++) {
-    const [,i,j] = all[k];
-    debug(js,i,j,k);
-    let e1 = js.find(e=>e.has(i));
-    let e2 = js.find(e=>e.has(j));
-    if (e1===e2) continue;
+const connectNext = (all,js) => {
+  const [,i,j] = all.shift();
+  let e1 = js.find(e=>e.has(i));
+  let e2 = js.find(e=>e.has(j));
+  if (e1!==e2) {
     const n1 = js.indexOf(e1);
     js.splice(n1,1);
     const n2 = js.indexOf(e2);
     js.splice(n2,1,e1.union(e2));
   }
-  return js.sort((a,b)=>b.size-a.size).slice(0,3).reduce((a,c)=>a*c.size,1);
+  return [i,j];
 }
 
-export const part2 = (input) => {
-  const all = getAllDist(input).sort(([a],[b])=>a-b);
-  const js = input.map((_,i)=>new Set([i]));
-  let k = 0;
-  while(js.length!==1) {
-    const [,i,j] = all[k++];
-    debug(js,i,j,k);
-    let e1 = js.find(e=>e.has(i));
-    let e2 = js.find(e=>e.has(j));
-    if (e1===e2) continue;
-    const n1 = js.indexOf(e1);
-    js.splice(n1,1);
-    const n2 = js.indexOf(e2);
-    js.splice(n2,1,e1.union(e2));
-  }
-  const [,n,m] = all[k-1];
-  return input[n][0]*input[m][0];
+export const part1 = ([,all,js]) => {
+  for (let k=isTest()?10:1000;k-->0;) 
+    connectNext(all,js);
+  return js.sort((a,b)=>b.size-a.size)
+    .slice(0,3).reduce((a,c)=>a*c.size,1);
+}
+
+export const part2 = ([input,all,js]) => {
+  let i,j;
+  while(js.length!==1)
+    [i,j] = connectNext(all,js);
+  return input[i][0]*input[j][0];
 };
 
 export const init = (data) =>
-  data.split('\n').map(r=>r.split(',').map(n=>+n));
+  (input=>[
+    input,
+    getAllDist(input).sort(([a],[b])=>a-b),
+    input.map((_,i)=>new Set([i]))
+  ])
+  (data.split('\n').map(r=>
+    r.split(',').map(n=>+n)));
